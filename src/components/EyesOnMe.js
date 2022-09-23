068953a3519955661ad77b667697609d0601941f
mercylynn <mercy.kamene@student.moringaschool.com>
2022-09-23 18:59:27 +0300

    Done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+function handleBlur() {
+    console.log('Hey! Eyes on me!')
+}
+function handleFocus() {
+    console.log('Good!')
+}
+
+function EyesOnMe() {
+    return (
+
+        <button type='button' onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe
